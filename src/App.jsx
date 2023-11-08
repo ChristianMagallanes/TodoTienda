ba905aa40077f788f5a92a7e24eb7e2b3fc14f86
christian magallanes <dar4del-x@hotmail.com>
2023-11-08 12:20:46 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
    <CartProvider>
       <BrowserRouter>
         <Navbar />
-        
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/item/:id" element={<ItemDetallesContainer />} />
